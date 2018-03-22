3b7bbd851b73d56e9f4bf2d41621543890cf5803
Ivan Ramirez <ivan.ramirez@axiacore.com>
2018-03-21 22:41:51 -0500

    tweak

--- platforms/android/app/src/main/assets/www/js/index.js
+++ platforms/android/app/src/main/assets/www/js/index.js
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -44,3 +27,64 @@ var app = {
 };
 
 app.initialize();
+
+var x = 0;
+var y = 0;
+$('.coordinates').append($('<div class="data"><input required class="x'+ x +' app-x" type="number" step="any" placeholder="X"><input required class="y' + y + ' app-y" type="number" step="any" placeholder="Y"></div>'));
+$('.new').on('click', function (){
+    x++;
+    y++;
+    $('.coordinates').append($('<div class="data"><input required class="x'+ x +' app-x" type="number" step="any" placeholder="X"><input required class="y' + y + ' app-y" type="number" step="any" placeholder="Y"></div>'));
+    if ($('.app-x').length == 3) {
+        $('.calculate').removeClass('hide');
+    }
+});
+
+$(".coordinates_form").submit(function(e) {
+    e.preventDefault();
+    var aux = 0;
+    var x_arr = []
+    var y_arr = []
+    for (var i = $('.app-x').length; i > 0; i--) {
+        data_x = $('.x'+aux).val();
+        data_y = $('.y'+aux).val();
+
+        if (!data_x == NaN || !data_x == '') {
+            x_arr.push(parseFloat(data_x));
+            y_arr.push(parseFloat(data_y));
+            aux++;
+        }
+    }
+    calculate(x_arr, y_arr)
+});
+    function calculate(x_arr, y_arr) {
+        var total_x = 0;
+        var total_y = 0;
+        var total_z = 0;
+        var a = 0;
+        var b = 1;
+        console.log(y_arr.length)
+        for (var i = x_arr.length; i > 1; i--) {
+            total_x = (x_arr[a] * y_arr[b]) + total_x
+            console.log('multx ' + x_arr[a] * y_arr[b] + ' ' + a + ' ' + b);
+            a++;
+            b++;
+        }
+        total_x = (x_arr[a] * y_arr[0]) + total_x
+        console.log('multx ' + x_arr[a] * y_arr[0] + ' ' + a + ' ' + 0);
+
+        a = 1;
+        b = 0;
+        for (var i = y_arr.length; i > 1; i--) {
+            total_y = (x_arr[a] * y_arr[b]) + total_y
+            console.log('multy ' + x_arr[a] * y_arr[b] + ' ' + a + ' ' + b);
+            a++;
+            b++;
+        }
+        total_y = (x_arr[0] * y_arr[b]) + total_y
+        console.log('multy ' + x_arr[0] * y_arr[b] + ' ' + 0 + ' ' + b + ' ' + x_arr[0] +' '+' '+ y_arr[b]);
+        console.log(total_x + ' ' + total_y)
+        total_z = (total_x - total_y) / 2
+        $('.hide').removeClass('hide');
+        $('.result').text(total_z + ' Kms 2').removeClass('hide');
+    }
